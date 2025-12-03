275399dcb724cca90886b311f29f53f7b1fec668
Dani <a@a.com>
2025-12-03 12:29:55 +0000

    Kata 3 created

---
+++ src/main/java/software/ulpgc/Main.java
@@ -0,0 +1,16 @@
+package software.ulpgc;
+
+import software.ulpgc.io.RemoteMovieLoader;
+import software.ulpgc.model.Movie;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        RemoteMovieLoader loader = new RemoteMovieLoader();
+        List<Movie> movie = loader.loadAll();
+        for (Movie m : movie) {
+            System.out.println(m);
+        }
+    }
+}
